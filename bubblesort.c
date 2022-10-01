f41cc8f96accb9210bc1b2b490483d64f67837d0
jeangondorek <jeangondorek73@gmail.com>
2022-09-30 22:04:34 -0300

    bubble sort

--- bubblesort.c
+++ bubblesort.c
@@ -1,8 +1,43 @@
 // bubble sort
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+    int count = 9, aux[count], lista[count];
+
+    for (int i = 0; i <= count; i++){
+        lista[i] = rand() % count+2;
+        aux[i] = lista[i];
+    }
+
+    for (int i = 0; i < count; i++){
+        printf("%d\t", lista[i]);
+    }
+
+    printf("\n");
+
+
+    for (int i = 0; i <= count; i++)
+    {
+        for (int j = i+1; j < count; j++)
+        {
+            if (lista [i] > lista[j])
+            {
+                lista[i] = lista[i+1];
+                lista[i] = lista[j];
+
+            }
+        }
+        
+    }
+
+
+
+    for (int i = 0; i < count; i++){
+        printf("%d\t", lista[i]);
+    }
+    printf("\n");
 
     return;
 }
